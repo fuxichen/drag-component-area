7093564c65560d93c49a3cc447408b35c5e4496f
BubbleTg <tg_z@qq.com>
2023-03-09 14:53:35 +0800

    fix: 解决dev启动报错，修改dev启动引用

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import DragComponentArea from '../dist/drag-component.umd.js'
+// import DragComponentArea from '../dist/drag-component.umd.js'
+import DragComponentArea from '../index.js'
 
 Vue.use(DragComponentArea);
 
